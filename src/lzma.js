08d826742d379215467ed9e63bda35bcfb812ef0
jcmellado <jcmellado@gmail.com>
2012-02-16 10:07:42 +0000

    Added new public function "decompressFile"

--- src/lzma.js
+++ src/lzma.js
@@ -1,3 +1,30 @@
+/*
+Copyright (c) 2011 Juan Mellado
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/*
+References:
+- "LZMA SDK" by Igor Pavlov
+  http://www.7-zip.org/sdk.html
+*/
 
 var LZMA = LZMA || {};
 
@@ -501,6 +528,30 @@ LZMA.decompress = function(properties, inStream, outStream, outSize){
   if ( !decoder.setDecoderProperties(properties) ){
     throw "Incorrect stream properties";
   }
+
+  if ( !decoder.decode(inStream, outStream, outSize) ){
+    throw "Error in data stream";
+  }
+
+  return true;
+};
+
+LZMA.decompressFile = function(inStream, outStream){
+  var decoder = new LZMA.Decoder(), outSize;
+
+  if ( !decoder.setDecoderProperties(inStream) ){
+    throw "Incorrect stream properties";
+  }
+
+  outSize = inStream.readByte();
+  outSize |= inStream.readByte() << 8;
+  outSize |= inStream.readByte() << 16;
+  outSize += inStream.readByte() * 16777216;
+
+  inStream.readByte();
+  inStream.readByte();
+  inStream.readByte();
+  inStream.readByte();
 
   if ( !decoder.decode(inStream, outStream, outSize) ){
     throw "Error in data stream";
